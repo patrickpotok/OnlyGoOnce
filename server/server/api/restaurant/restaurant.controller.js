432de842b93a8778ec4c7c7bff22645c8a57807f
Justin Yang <justin.yang@adroll.com>
2015-10-11 00:24:24 -0700

    rando time

--- server/server/api/restaurant/restaurant.controller.js
+++ server/server/api/restaurant/restaurant.controller.js
@@ -8,6 +8,46 @@ var User = require('./../user/user.model');
 var Restaurant = require('./../restaurant/restaurant.model');
 var History = require('../history/history.model');
 
+// Taken from: https://raw.githubusercontent.com/kvz/phpjs/master/functions/array/array_rand.js
+function array_rand(input, num_req) {
+  //  discuss at: http://phpjs.org/functions/array_rand/
+  // original by: Waldo Malqui Silva (http://waldo.malqui.info)
+  //   example 1: array_rand( ['Kevin'], 1 );
+  //   returns 1: 0
+
+  var indexes = [];
+  var ticks = num_req || 1;
+  var checkDuplicate = function(input, value) {
+    var exist = false,
+      index = 0,
+      il = input.length;
+    while (index < il) {
+      if (input[index] === value) {
+        exist = true;
+        break;
+      }
+      index++;
+    }
+    return exist;
+  };
+
+  if (Object.prototype.toString.call(input) === '[object Array]' && ticks <= input.length) {
+    while (true) {
+      var rand = Math.floor((Math.random() * input.length));
+      if (indexes.length === ticks) {
+        break;
+      }
+      if (!checkDuplicate(indexes, rand)) {
+        indexes.push(rand);
+      }
+    }
+  } else {
+    indexes = null;
+  }
+
+  return ((ticks == 1) ? indexes.join() : indexes);
+}
+
 // Get list of histories
 exports.index = function(req, res) {
   var userId = req.user && req.user._id;
@@ -40,15 +80,19 @@ exports.index = function(req, res) {
                 countFiltered++;
               }
           }
-
           for(var i = 0; i < result.results.items.length; i++){
             if(filter.indexOf(result.results.items[i].id) == -1){ //Not in filter
               result_list[i] = result.results.items[i];
-              if(result_list.length == number){
-                break;
-              }
             }
           }
+
+          var answer = [];
+          var rando = array_rand(result_list, number);
+          for (var i = 0; i < rando.length; i++) {
+            answer[i] = result_list[rando[i]];
+          }
+          result_list = answer;
+
           // Store Restaurant information if need to. Make more efficient in future
           // Currently stores as we query
           for (var i = 0; i < result.results.items.length; i++){
