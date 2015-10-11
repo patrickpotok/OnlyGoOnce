04217ff9e8fb42bfbc2150fedf95e6d51cdaf8d8
Patrick Potok <patrick.potok@adroll.com>
2015-10-10 18:09:35 -0700

    Added code for HERE Map

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -28,9 +28,38 @@ angular.module('starter.controllers', [])
   };
 })
 
-// .controller('RestaurantCtrl', function($scope) {
+.controller('RestaurantCtrl', function($scope) {
+  $scope.setMap = function(map) {
+    map.setCenter({
+      lat:52.5159,
+      lng:13.3777
+    });
+    map.setZoom(14);
+  }
 
-// })
+  //Step 1: initialize communication with the platform
+  var platform = new H.service.Platform({
+    app_id: 'evk3TrU4UcresAseG8Da',
+    app_code: 'z4yYohROherMZ57eHTsQUg',
+    useCIT: true,
+    useHTTPS: true
+  });
+  var defaultLayers = platform.createDefaultLayers();
+
+  //Step 2: initialize a map  - not specificing a location will give a whole world view.
+  var map = new H.Map(document.getElementById('map'), defaultLayers.normal.map);
+
+  //Step 3: make the map interactive
+  // MapEvents enables the event system
+  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+  // Create the default UI components
+  var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+  // Now use the map as required...
+  setMap(map);
+})
 
 // .controller('BrowseHereCtrl', function($scope) {
 
