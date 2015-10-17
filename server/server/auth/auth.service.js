3b39df4be80c371472ac19a72ec3274f3178b218
Ryan Ly <windask@hotmail.com>
2015-10-17 04:20:31 -0700

    Forgot brackets

--- server/server/auth/auth.service.js
+++ server/server/auth/auth.service.js
@@ -68,7 +68,7 @@ function setTokenCookie(req, res) {
   if (!req.user) return res.status(404).json({ message: 'Something went wrong, please try again.'});
   var token = signToken(req.user._id, req.user.role);
   //res.cookie('token', JSON.stringify(token));
-  if process.env.CLIENT_NODE_ENV{
+  if (process.env.CLIENT_NODE_ENV) {
     res.redirect('http://www.onlygoonce.com/#/authToken?token=' + token);
   }
   else{
